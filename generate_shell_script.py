a04b7c854310ab29207e72349dc2e255ae00e3e8
Keenan Gebze <keenan.gebze@gmail.com>
2018-06-05 10:16:21 +0800

    rename generated script to start

--- generate_shell_script.py
+++ generate_shell_script.py
@@ -6,6 +6,6 @@ with open("cctv.csv", "r") as file:
     i = 0
     for row in reader:
         i = i + 1
-        with open("script"+str(i)+".sh", "w") as out:
+        with open("start"+str(i)+".sh", "w") as out:
             out.write("python3 cctv_object_detection.py "+" ".join('"'+r+'"' for r in row))
     
